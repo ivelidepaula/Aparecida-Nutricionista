667b9b0f571d12b7ec0af4cefab0bcfc6a904702
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-10-27 16:38:48 +0100

    Fade out when element is removed

--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -2,6 +2,8 @@ var tabela = document.querySelector('#tabela-pacientes');
 
 tabela.addEventListener('dblclick', function(event) {
     if (event.target.tagName == 'TD') {
-        event.target.parentNode.remove()
-    }
+        event.target.parentNode.classList.add("fadeOut");
+        setTimeout(function() {
+            event.target.parentNode.remove();
+        }, 500);    }
 });
