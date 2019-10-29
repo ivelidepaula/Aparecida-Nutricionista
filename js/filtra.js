015682348937f239161f72539f794844a1b29037
Iveli de Paula Sousa <iveli.sousa@gmail.com>
2019-10-29 08:55:49 +0100

    Add filter

--- js/filtra.js
+++ js/filtra.js
@@ -5,8 +5,16 @@ campoFiltro.addEventListener("input", function() {
 
     if (this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++) {
-            var paciente = pacientes[i];            
-            paciente.classList.add("invisivel");    
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+
+            if (expressao.test(nome)) {
+                paciente.classList.remove("invisivel");
+            } else {
+                paciente.classList.add("invisivel");
+            }
         }
     } else {
         for (var i = 0; i < pacientes.length; i++) {
